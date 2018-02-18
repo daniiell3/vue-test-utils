96ed806830fc521933ade1fe6f28402b4338470e
Daniel Santos <daniel@justdigital.com.br>
2018-02-18 16:47:41 -0300

    setup test dentist

---
+++ test/unit/specs/Dentist.spec.js
@@ -0,0 +1,64 @@
+import { shallow, mount, createLocalVue } from '@vue/test-utils'
+import { expect } from 'chai'
+import Vuex from 'vuex'
+import Dentist from '@/components/Dentist/Index.vue'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+describe('CancelConfirm.vue', () => {
+  let store
+  const state = {
+    isActive: true,
+    item: {
+      name: 'Cristina Yuri Shigenaga',
+      cro: '44765',
+      providerType: 'Consultório Isolado - Odontológico Pessoa Física',
+      phone: '(11) 2041-0821',
+      specialties: [
+        {
+          type: 'consultation',
+          title: 'Clínico Geral'
+        },
+        {
+          type: 'surgery',
+          title: 'Cirurgia'
+        },
+        {
+          type: 'pediatric-dentistry',
+          title: 'Odontopediatria'
+        }
+      ],
+      address: {
+        postcode: '03881-170',
+        city: 'São Paulo',
+        state: 'SP',
+        description: 'Rua Sargento Evangelista, 05 SL 02',
+        neighborhood: 'Ponte Rasa'
+      },
+      scheduleUrl: 'https://redeunna.boaconsulta.com/silvio-rogerio-falcao-monteiro?utm_source=odontoprev&utm_medium=button&utm_campaign=silvio-rogerio-falcao-monteiro'
+    },
+    color: '#1974ce'
+  }
+
+  const getters = {
+    isActive: state => state.isActive,
+    item: state => state.item,
+    color: state => state.color
+  }
+
+  describe('Default', () => {
+    beforeEach(() => {
+      store = new Vuex.Store({
+        modules: {
+          dentist: {
+            namespaced: true,
+            state: state,
+            getters: getters
+          }
+        },
+        strict: false
+      })
+    })
+  })
+})
