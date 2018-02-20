5d21e69a60d514e68307e03567449cc6102575f2
Daniel Santos <daniel@justdigital.com.br>
2018-02-19 23:11:05 -0300

    ajuste em start de teste de componente order

--- test/unit/specs/Order.spec.js
+++ test/unit/specs/Order.spec.js
@@ -3,25 +3,23 @@ import { expect } from 'chai'
 import sinon from 'sinon'
 import Order from '@/components/Order/Index.vue'
 
-describe('Order.vue', () => {
-  const propsData = {
-    orderStatus: 'payment_review',
-    plan: {
-      icon: 'https://odontoprevonline.com.br/sites/default/files/styles/thumbnail/public/2017-08/odontoprev-dental-estetica.png?itok=93uFTNDG',
-      title: 'Dental Estética'
-    },
-    beneficiaries: {
-      main: 'Daniel Santos',
-      dependents: []
-    },
-    documents: {
-      proposal: 'https://vue-test-utils.vuejs.org/en/api/wrapper',
-      contract: 'https://vue-test-utils.vuejs.org/en/api/wrapper-array/'
-    },
-    handleCancel: () => {}
-  }
+const propsData = {
+  orderStatus: 'payment_review',
+  plan: {
+    icon: 'https://odontoprevonline.com.br/sites/default/files/styles/thumbnail/public/2017-08/odontoprev-dental-estetica.png?itok=93uFTNDG',
+    title: 'Dental Estética'
+  },
+  beneficiaries: {
+    main: 'Daniel Santos',
+    dependents: []
+  },
+  documents: {
+    proposal: 'https://vue-test-utils.vuejs.org/en/api/wrapper',
+    contract: 'https://vue-test-utils.vuejs.org/en/api/wrapper-array/'
+  },
+  handleCancel: () => {}
+}
 
-  const wrapper = shallow(Order, {
-    propsData
-  })
+const wrapper = shallow(Order, {
+  propsData
 })
