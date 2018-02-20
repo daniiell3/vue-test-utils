a61dc22f04b03e212d0035d28eea8494dadfa332
Daniel Santos <daniel@justdigital.com.br>
2018-02-19 22:52:52 -0300

    altera descricao dos testes em componente cancel confirm

--- test/unit/specs/CancelConfirm.spec.js
+++ test/unit/specs/CancelConfirm.spec.js
@@ -3,80 +3,78 @@ import { expect } from 'chai'
 import sinon from 'sinon'
 import CancelConfirm from '@/components/CancelConfirm/Index.vue'
 
-describe('CancelConfirm.vue', () => {
-  const propsData = {
-    isActive: true,
-    title: 'Dental Estética',
-    dependents: [],
-    cancel: () => {},
-    proceed: () => {}
-  }
+const propsData = {
+  isActive: true,
+  title: 'Dental Estética',
+  dependents: [],
+  cancel: () => {},
+  proceed: () => {}
+}
 
-  const wrapper = mount(CancelConfirm, {
-    propsData
+const wrapper = mount(CancelConfirm, {
+  propsData
+})
+
+it('title correctly rendered', () => {
+  const titleEl = wrapper.find('.qa-confirm-cancel__title')
+
+  expect(titleEl.text()).to.include(propsData.title.toUpperCase())
+})
+
+it('does not render dependents when passed dependents prop as empty', () => {
+  const dependentsEl = wrapper.find('.qa-confirm-cancel__dependents-wrap')
+
+  expect(dependentsEl.exists()).to.be.false
+})
+
+it('dependents correctly rendered when passed dependents prop', () => {
+  const dependents = ['Daniela Souza', 'Maria Tereza']
+
+  wrapper.setProps({
+    dependents: dependents
   })
 
-  it('title |> text()', () => {
-    const titleEl = wrapper.find('.qa-confirm-cancel__title')
+  const dependentItemsEl = wrapper.find('.qa-confirm-cancel__items')
 
-    expect(titleEl.text()).to.include(propsData.title.toUpperCase())
+  expect(dependentItemsEl.exists()).to.be.true
+  expect(dependentItemsEl.text()).to.deep.equal(dependents.join(''))
+})
+
+it('calls clickHandler when cancel button is clicked', () => {
+  const clickHandler = sinon.stub()
+
+  wrapper.setProps({
+    cancel: clickHandler
   })
 
-  it('not dependents |> exists()', () => {
-    const dependentsEl = wrapper.find('.qa-confirm-cancel__dependents-wrap')
+  expect(clickHandler.called).to.be.false
 
-    expect(dependentsEl.exists()).to.be.false
+  const buttonEl = wrapper.find('.qa-confirm-cancel__action--no button')
+  buttonEl.trigger('click')
+
+  expect(clickHandler.called).to.be.true
+})
+
+it('calls clickHandler when proceed button is clicked', () => {
+  const clickHandler = sinon.stub()
+
+  wrapper.setProps({
+    proceed: clickHandler
   })
 
-  it('dependents |> exists(), text()', () => {
-    const dependents = ['Daniela Souza', 'Maria Tereza']
+  expect(clickHandler.called).to.be.false
 
-    wrapper.setProps({
-      dependents: dependents
-    })
+  const buttonEl = wrapper.find('.qa-confirm-cancel__action--yes button')
+  buttonEl.trigger('click')
 
-    const dependentItemsEl = wrapper.find('.qa-confirm-cancel__items')
+  expect(clickHandler.called).to.be.true
+})
 
-    expect(dependentItemsEl.exists()).to.be.true
-    expect(dependentItemsEl.text()).to.deep.equal(dependents.join(''))
+it('does not render when passed active prop as false', () => {
+  wrapper.setProps({
+    isActive: false
   })
 
-  it('cancel |> trigger()', () => {
-    const clickHandler = sinon.stub()
-
-    wrapper.setProps({
-      cancel: clickHandler
-    })
-
-    expect(clickHandler.called).to.be.false
-
-    const buttonEl = wrapper.find('.qa-confirm-cancel__action--no button')
-    buttonEl.trigger('click')
-
-    expect(clickHandler.called).to.be.true
-  })
-
-  it('proceed |> trigger()', () => {
-    const clickHandler = sinon.stub()
-
-    wrapper.setProps({
-      proceed: clickHandler
-    })
-
-    expect(clickHandler.called).to.be.false
-
-    const buttonEl = wrapper.find('.qa-confirm-cancel__action--yes button')
-    buttonEl.trigger('click')
-
-    expect(clickHandler.called).to.be.true
-  })
-
-  it('active |> exists()', () => {
-    wrapper.setProps({
-      isActive: false
-    })
-
-    const cancelEl = wrapper.find('.qa-confirm-cancel')
-    expect(cancelEl.exists()).to.be.false
-  })
+  const cancelEl = wrapper.find('.qa-confirm-cancel')
+  expect(cancelEl.exists()).to.be.false
 })
