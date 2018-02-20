1c0ae8f85a2a711291da2a126373b51da19edb83
Daniel Santos <daniel@justdigital.com.br>
2018-02-19 23:25:29 -0300

    ajuste em sexto cenario de componente order

--- test/unit/specs/Order.spec.js
+++ test/unit/specs/Order.spec.js
@@ -3,68 +3,106 @@ import { expect } from 'chai'
 import sinon from 'sinon'
 import Order from '@/components/Order/Index.vue'
 
-describe('Order.vue', () => {
-  const propsData = {
-    orderStatus: 'payment_review',
-    plan: {
-      icon: 'https://odontoprevonline.com.br/sites/default/files/styles/thumbnail/public/2017-08/odontoprev-dental-estetica.png?itok=93uFTNDG',
-      title: 'Dental Estética'
-    },
-    beneficiaries: {
-      main: 'Daniel Santos',
-      dependents: []
-    },
-    documents: {
-      proposal: 'https://vue-test-utils.vuejs.org/en/api/wrapper',
-      contract: 'https://vue-test-utils.vuejs.org/en/api/wrapper-array/'
-    },
-    handleCancel: () => {}
-  }
+const propsData = {
+  orderStatus: 'payment_review',
+  plan: {
+    icon: 'https://odontoprevonline.com.br/sites/default/files/styles/thumbnail/public/2017-08/odontoprev-dental-estetica.png?itok=93uFTNDG',
+    title: 'Dental Estética'
+  },
+  beneficiaries: {
+    main: 'Daniel Santos',
+    dependents: []
+  },
+  documents: {
+    proposal: 'https://vue-test-utils.vuejs.org/en/api/wrapper',
+    contract: 'https://vue-test-utils.vuejs.org/en/api/wrapper-array/'
+  },
+  handleCancel: () => {}
+}
 
-  const wrapper = shallow(Order, {
-    propsData
+const wrapper = shallow(Order, {
+  propsData
+})
+
+it('icon correctly rendered', () => {
+  const iconEl = wrapper.find('.qa-orders__icon')
+  const src = iconEl.attributes().src
+
+  expect(src).to.deep.equal(propsData.plan.icon)
+})
+
+it('title correctly rendered', () => {
+  const titleEl = wrapper.find('.qa-orders__title')
+
+  expect(titleEl.text()).to.deep.equal(propsData.plan.title)
+})
+
+it('proposal correctly rendered', () => {
+  const documentsAnchorEl = wrapper.findAll('.qa-orders__documents > a')
+  const proposalEl = documentsAnchorEl.filter(i => i.text() === 'Ver Proposta').at(0)
+  const href = proposalEl.attributes().href
+
+  expect(href).to.deep.equal(propsData.documents.proposal)
+})
+
+it('does not render dependents when not passed dependents prop', () => {
+  const dependentsEl = wrapper.find('.qa-orders__dependents__items')
+  expect(dependentsEl.exists()).to.be.false
+
+  const dependentsEmptyEl = wrapper.find('span.qa-orders__dependents__item')
+  expect(dependentsEmptyEl.exists()).to.be.true
+})
+
+it('dependents correctly rendered when passed dependents prop', () => {
+  const dependents = ['Daniela Souza', 'Maria Tereza']
+
+  const newWrapper = shallow(Order, {
+    propsData: Object.assign({}, propsData, {
+      beneficiaries: Object.assign({}, propsData.beneficiaries, {
+        dependents: dependents
+      })
+    })
   })
 
-  it('icon |> attributes()', () => {
-    const iconEl = wrapper.find('.qa-orders__icon')
-    const src = iconEl.attributes().src
+  const dependentItemsEl = newWrapper.find('.qa-orders__dependents__items')
+  expect(dependentItemsEl.exists()).to.be.true
+  expect(dependentItemsEl.text()).to.deep.equal(dependents.join(''))
 
-    expect(src).to.deep.equal(propsData.plan.icon)
+  const dependentsEmptyEl = newWrapper.find('span.qa-orders__dependents__item')
+  expect(dependentsEmptyEl.exists()).to.be.false
+})
+
+it('calls clickHandler when cancel button is clicked', () => {
+  const clickHandler = sinon.stub()
+
+  const newProps = Object.assign({}, propsData, {
+    orderStatus: 'complete',
+    handleCancel: clickHandler
   })
 
-  it('title |> text()', () => {
-    const titleEl = wrapper.find('.qa-orders__title')
-
-    expect(titleEl.text()).to.deep.equal(propsData.plan.title)
+  const newWrapper = mount(Order, {
+    propsData: newProps
   })
 
-  it('main |> text()', () => {
-    const nameEl = wrapper.find('.qa-orders__main__name')
+  expect(clickHandler.called).to.be.false
 
-    expect(nameEl.text()).to.deep.equal(propsData.beneficiaries.main)
+  const buttonEl = newWrapper.find('.qa-orders__cancel-plan button')
+  buttonEl.trigger('click')
+
+  expect(clickHandler.called).to.be.true
+})
+
+it('correctly rendered when passed order status prop as canceled', () => {
+  wrapper.setProps({
+    orderStatus: 'canceled'
   })
 
-  it('not dependents |> exists()', () => {
-    const dependentsEl = wrapper.find('.qa-orders__dependents__items')
-    expect(dependentsEl.exists()).to.be.false
+  const orderItemsEl = wrapper.find('.qa-orders')
+  expect(orderItemsEl.classes()).to.include('disabled')
 
-    const dependentsEmptyEl = wrapper.find('span.qa-orders__dependents__item')
-    expect(dependentsEmptyEl.exists()).to.be.true
-  })
+  const actionsEl = wrapper.find('.qa-orders__actions')
+  expect(actionsEl.exists()).to.be.false
 
-  it('proposal |> findAll(), filter(), text(), attributes(), at()', () => {
-    const documentsAnchorEl = wrapper.findAll('.qa-orders__documents > a')
-    const proposalEl = documentsAnchorEl.filter(i => i.text() === 'Ver Proposta').at(0)
-    const href = proposalEl.attributes().href
-
-    expect(href).to.deep.equal(propsData.documents.proposal)
-  })
-
-  it('contract |> findAll(), filter(), text(), attributes(), at()', () => {
-    const documentsAnchorEl = wrapper.findAll('.qa-orders__documents > a')
-    const contractEl = documentsAnchorEl.filter(i => i.text() === 'Ver Contrato').at(0)
-    const href = contractEl.attributes().href
-
-    expect(href).to.deep.equal(propsData.documents.contract)
-  })
+  const canceledEl = wrapper.find('.qa-orders__canceled-text')
+  expect(canceledEl.exists()).to.be.true
 })
