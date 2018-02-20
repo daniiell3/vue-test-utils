59242f4caa1ef30d695c0019ab58f22c9aa793be
Daniel Santos <daniel@justdigital.com.br>
2018-02-19 22:58:33 -0300

    ajuste em segundo cenario de componente dentist

--- test/unit/specs/Dentist.spec.js
+++ test/unit/specs/Dentist.spec.js
@@ -6,81 +6,80 @@ import Dentist from '@/components/Dentist/Index.vue'
 const localVue = createLocalVue()
 localVue.use(Vuex)
 
-describe('CancelConfirm.vue', () => {
-  let store
-  const state = {
-    isActive: true,
-    item: {
-      name: 'Cristina Yuri Shigenaga',
-      cro: '44765',
-      providerType: 'Consultório Isolado - Odontológico Pessoa Física',
-      phone: '(11) 2041-0821',
-      specialties: [
-        {
-          type: 'consultation',
-          title: 'Clínico Geral'
-        },
-        {
-          type: 'surgery',
-          title: 'Cirurgia'
-        },
-        {
-          type: 'pediatric-dentistry',
-          title: 'Odontopediatria'
-        }
-      ],
-      address: {
-        postcode: '03881-170',
-        city: 'São Paulo',
-        state: 'SP',
-        description: 'Rua Sargento Evangelista, 05 SL 02',
-        neighborhood: 'Ponte Rasa'
+let store
+
+const state = {
+  isActive: true,
+  item: {
+    name: 'Cristina Yuri Shigenaga',
+    cro: '44765',
+    providerType: 'Consultório Isolado - Odontológico Pessoa Física',
+    phone: '(11) 2041-0821',
+    specialties: [
+      {
+        type: 'consultation',
+        title: 'Clínico Geral'
       },
-      scheduleUrl: 'https://redeunna.boaconsulta.com/silvio-rogerio-falcao-monteiro?utm_source=odontoprev&utm_medium=button&utm_campaign=silvio-rogerio-falcao-monteiro'
+      {
+        type: 'surgery',
+        title: 'Cirurgia'
+      },
+      {
+        type: 'pediatric-dentistry',
+        title: 'Odontopediatria'
+      }
+    ],
+    address: {
+      postcode: '03881-170',
+      city: 'São Paulo',
+      state: 'SP',
+      description: 'Rua Sargento Evangelista, 05 SL 02',
+      neighborhood: 'Ponte Rasa'
     },
-    color: '#1974ce'
-  }
+    scheduleUrl: 'https://redeunna.boaconsulta.com/silvio-rogerio-falcao-monteiro?utm_source=odontoprev&utm_medium=button&utm_campaign=silvio-rogerio-falcao-monteiro'
+  },
+  color: '#1974ce'
+}
 
-  const getters = {
-    isActive: state => state.isActive,
-    item: state => state.item,
-    color: state => state.color
-  }
+const getters = {
+  isActive: state => state.isActive,
+  item: state => state.item,
+  color: state => state.color
+}
 
-  describe('Default', () => {
-    beforeEach(() => {
-      store = new Vuex.Store({
-        modules: {
-          dentist: {
-            namespaced: true,
-            state: state,
-            getters: getters
-          }
-        },
-        strict: false
-      })
+describe('default', () => {
+  beforeEach(() => {
+    store = new Vuex.Store({
+      modules: {
+        dentist: {
+          namespaced: true,
+          state: state,
+          getters: getters
+        }
+      },
+      strict: false
     })
+  })
 
-    it('name |> text()', () => {
-      const wrapper = shallow(Dentist, {
-        store,
-        localVue
-      })
-      const titleEl = wrapper.find('.qa-dentist__title')
-
-      expect(titleEl.text()).to.deep.equal(`Dr(a). ${state.item.name}`)
+  it('name correctly rendered', () => {
+    const wrapper = shallow(Dentist, {
+      store,
+      localVue
     })
+    const titleEl = wrapper.find('.qa-dentist__title')
 
-    it('info |> text()', () => {
-      const wrapper = shallow(Dentist, {
-        store,
-        localVue
-      })
-      const textEl = wrapper.find('.qa-dentist__info')
-      const text = textEl.text()
+    expect(titleEl.text()).to.deep.equal(`Dr(a). ${state.item.name}`)
+  })
 
-      expect(text).to.include(state.item.cro)
-      expect(text).to.include(state.item.providerType)
+  it('info correctly rendered', () => {
+    const wrapper = shallow(Dentist, {
+      store,
+      localVue
     })
+    const textEl = wrapper.find('.qa-dentist__info')
+    const text = textEl.text()
+
+    expect(text).to.include(state.item.cro)
+    expect(text).to.include(state.item.providerType)
   })
 })
