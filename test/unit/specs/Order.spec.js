afccfd0b3104b22fb2a2dd1783cc9a24b96210c0
Daniel Santos <daniel@justdigital.com.br>
2018-02-19 23:15:45 -0300

    ajuste em terceiro cenario de componente order

--- test/unit/specs/Order.spec.js
+++ test/unit/specs/Order.spec.js
@@ -3,44 +3,44 @@ import { expect } from 'chai'
 import sinon from 'sinon'
 import Order from '@/components/Order/Index.vue'
 
-describe('Order.vue', () => {
-  const propsData = {
-    orderStatus: 'payment_review',
-    plan: {
-      icon: 'https://odontoprevonline.com.br/sites/default/files/styles/thumbnail/public/2017-08/odontoprev-dental-estetica.png?itok=93uFTNDG',
-      title: 'Dental Estética'
-    },
-    beneficiaries: {
-      main: 'Daniel Santos',
-      dependents: []
-    },
-    documents: {
-      proposal: 'https://vue-test-utils.vuejs.org/en/api/wrapper',
-      contract: 'https://vue-test-utils.vuejs.org/en/api/wrapper-array/'
-    },
-    handleCancel: () => {}
-  }
+const propsData = {
+  orderStatus: 'payment_review',
+  plan: {
+    icon: 'https://odontoprevonline.com.br/sites/default/files/styles/thumbnail/public/2017-08/odontoprev-dental-estetica.png?itok=93uFTNDG',
+    title: 'Dental Estética'
+  },
+  beneficiaries: {
+    main: 'Daniel Santos',
+    dependents: []
+  },
+  documents: {
+    proposal: 'https://vue-test-utils.vuejs.org/en/api/wrapper',
+    contract: 'https://vue-test-utils.vuejs.org/en/api/wrapper-array/'
+  },
+  handleCancel: () => {}
+}
 
-  const wrapper = shallow(Order, {
-    propsData
-  })
+const wrapper = shallow(Order, {
+  propsData
+})
 
-  it('icon |> attributes()', () => {
-    const iconEl = wrapper.find('.qa-orders__icon')
-    const src = iconEl.attributes().src
+it('icon correctly rendered', () => {
+  const iconEl = wrapper.find('.qa-orders__icon')
+  const src = iconEl.attributes().src
 
-    expect(src).to.deep.equal(propsData.plan.icon)
-  })
+  expect(src).to.deep.equal(propsData.plan.icon)
+})
 
-  it('title |> text()', () => {
-    const titleEl = wrapper.find('.qa-orders__title')
+it('title correctly rendered', () => {
+  const titleEl = wrapper.find('.qa-orders__title')
 
-    expect(titleEl.text()).to.deep.equal(propsData.plan.title)
-  })
+  expect(titleEl.text()).to.deep.equal(propsData.plan.title)
+})
 
-  it('main |> text()', () => {
-    const nameEl = wrapper.find('.qa-orders__main__name')
+it('proposal correctly rendered', () => {
+  const documentsAnchorEl = wrapper.findAll('.qa-orders__documents > a')
+  const proposalEl = documentsAnchorEl.filter(i => i.text() === 'Ver Proposta').at(0)
+  const href = proposalEl.attributes().href
 
-    expect(nameEl.text()).to.deep.equal(propsData.beneficiaries.main)
-  })
+  expect(href).to.deep.equal(propsData.documents.proposal)
 })
